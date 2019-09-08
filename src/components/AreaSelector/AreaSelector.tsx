f5e5080a2998e5f8ec0a516be75f4cd8c2b58640
Philippe Ottenwaelter <pottenwaelter@free.fr>
2019-09-08 19:03:19 +0200

    Fix coordinate problem in Area Selector

--- src/components/AreaSelector/AreaSelector.tsx
+++ src/components/AreaSelector/AreaSelector.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 
-type Props = {
+export type AreaSelectorProps = {
   x: number;
   y: number;
   width: number;
   height: number;
 };
 
-export default function AreaSelector(props: Props) {
+export default function AreaSelector(props: AreaSelectorProps) {
+  let { x, y, width, height } = props;
+  if (width < 0) {
+    width = Math.abs(width);
+    x -= width;
+  }
+  if (height < 0) {
+    height = Math.abs(height);
+    y -= height;
+  }
+
   const AreaContainer = styled.div`
     position: relative;
     width: 100%;
@@ -20,10 +30,10 @@ export default function AreaSelector(props: Props) {
     background: rgba(255, 255, 255, 0.2);
     z-index: 9999;
     pointer-events: none;
-    top: ${props.y}px;
-    left: ${props.x}px;
-    width: ${props.width}px;
-    height: ${props.height}px;
+    top: ${y}px;
+    left: ${x}px;
+    width: ${width}px;
+    height: ${height}px;
   `;
   return (
     <AreaContainer>
