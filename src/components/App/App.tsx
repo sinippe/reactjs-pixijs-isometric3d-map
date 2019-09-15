7547e37717fa7695f18978276798626121065120
Philippe Ottenwaelter <pottenwaelter@free.fr>
2019-09-15 16:38:25 +0200

    Update component App: made it a function component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import CanvasContainer from '../CanvasContainer/CanvasContainer';
-import MapSelector from '../MapSelector/MapSelector';
 import styled from 'styled-components';
 import Progress from '../Progress/Progress';
+import MapSelector from '../MapSelector/MapSelector';
 import CustomDialog from '../CustomDialog/CustomDialog';
 
 const files = [
@@ -17,104 +17,77 @@ const ButtonToIsometric = styled.button`
   top: 1em;
   right: 1em;
 `;
-const ButtonToMap = styled.button`
+const ButtonToMapSelector = styled.button`
   top: 1em;
   left: 1em;
 `;
 
 type Props = {};
-type State = {
-  activeFileIndex: number;
-  mapData: JSON | undefined;
-  displayMap: boolean;
-  isLoading: boolean;
-  mapFirstVisit: boolean;
-  displayMapWarning: boolean;
-};
 
-class App extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      activeFileIndex: 0,
-      mapData: undefined,
-      displayMap: false,
-      isLoading: false,
-      mapFirstVisit: true,
-      displayMapWarning: true
-    };
+export default function App(props: Props) {
+  const [activeFileIndex, setActiveFileIndex] = useState(0);
+  const [mapData, setMapData] = useState<JSON | undefined>(undefined);
+  const [displayMapSelector, setDisplayMapSelector] = useState(false);
+  const [mapVisits, setMapVisits] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
-    this.displayIsometric3dView = this.displayIsometric3dView.bind(this);
-    this.displayMap = this.displayMap.bind(this);
-    this.onMapDataLoadInit = this.onMapDataLoadInit.bind(this);
-    this.onMapData = this.onMapData.bind(this);
-  }
+  const onMapData = (data: JSON) => {
+    setIsLoading(false);
+    setMapData(data);
+  };
 
-  displayIsometric3dView() {
-    this.setState({ displayMap: false });
-  }
+  useEffect(() => {
+    setDisplayMapSelector(false);
+    setIsLoading(false);
+  }, [mapData]);
 
-  displayMap() {
-    const newState: any = { displayMap: true };
-    if (this.state.mapFirstVisit) {
-      newState.mapFirstVisit = false;
-    } else if (this.state.displayMapWarning) {
-      newState.displayMapWarning = false;
+  useEffect(() => {
+    if (displayMapSelector) {
+      setMapVisits(mapVisits + 1);
     }
-    this.setState(newState);
-  }
+  }, [displayMapSelector]);
 
-  onMapDataLoadInit() {
-    this.setState({ isLoading: true });
-  }
-
-  onMapData(data: JSON) {
-    this.setState({
-      mapData: data,
-      displayMap: false,
-      isLoading: false
-    });
-  }
-
-  render() {
-    return (
-      <>
-        {this.state.displayMap && this.state.displayMapWarning && (
-          <CustomDialog content="Drag around the map to select the area to render. Click once to start drawing the area. Click once more to end drawing."></CustomDialog>
-        )}
-        {this.state.isLoading && <Progress></Progress>}
-        {this.state.displayMap && (
-          <>
-            <ButtonToIsometric
-              className="switch-view"
-              onClick={this.displayIsometric3dView}
-            >
-              Isometric 3d View &gt;
-            </ButtonToIsometric>
-            <MapSelector
-              onFetchDataInit={this.onMapDataLoadInit}
-              onFetchData={this.onMapData}
-            />
-          </>
-        )}
-        {!this.state.displayMap && (
-          <>
-            <ButtonToMap
-              className="switch-view inverted"
-              onClick={this.displayMap}
-            >
-              &lt; Map
-            </ButtonToMap>
-            <CanvasContainer
-              key={`mapData${this.state.activeFileIndex}`}
-              file={files[this.state.activeFileIndex].file}
-              data={this.state.mapData}
-            />
-          </>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      {isLoading && <Progress></Progress>}
+      {displayMapSelector && mapVisits === 1 && (
+        <CustomDialog content="Drag around the map to select the area to render. Click once to start drawing the area. Click once more to end drawing."></CustomDialog>
+      )}
+      {displayMapSelector && (
+        <>
+          <ButtonToIsometric
+            className="switch-view"
+            onClick={() => {
+              setDisplayMapSelector(false);
+            }}
+          >
+            Isometric 3d View &gt;
+          </ButtonToIsometric>
+          <MapSelector
+            onFetchDataInit={() => {
+              setIsLoading(true);
+            }}
+            onFetchData={onMapData}
+          />
+        </>
+      )}
+      {!displayMapSelector && (
+        <>
+          <ButtonToMapSelector
+            className="switch-view inverted"
+            onClick={() => {
+              setDisplayMapSelector(true);
+            }}
+          >
+            &lt; Map
+          </ButtonToMapSelector>
+          <CanvasContainer
+            key={`mapData${activeFileIndex}`}
+            file={files[activeFileIndex].file}
+            data={mapData}
+          />
+        </>
+      )}
+    </>
+  );
 }
-
-export default App;
