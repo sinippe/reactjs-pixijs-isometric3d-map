c73b03243a9829b84bd7081bc2a3807c21ca7f59
Philippe Ottenwaelter <pottenwaelter@free.fr>
2019-09-18 21:46:19 +0200

    Add MapMenu implementation in App + Disable button to MapSelector

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import styled from 'styled-components';
 import './App.scss';
 import CanvasContainer from '../CanvasContainer/CanvasContainer';
-import styled from 'styled-components';
 import Progress from '../Progress/Progress';
 import MapSelector from '../MapSelector/MapSelector';
 import CustomDialog from '../CustomDialog/CustomDialog';
+import MapMenu from '../MapMenu/MapMenu';
 import config from '../../config/infos.json';
 
 const files = [
@@ -27,6 +28,12 @@ const ButtonToIsometric = styled.button`
   right: 1em;
 `;
 const ButtonToMapSelector = styled.button`
+  top: 4em;
+  left: 1em;
+  cursor: not-allowed !important;
+  opacity: 0.5;
+`;
+const ButtonToMapMenu = styled.button`
   top: 1em;
   left: 1em;
 `;
@@ -34,9 +41,10 @@ const ButtonToMapSelector = styled.button`
 type Props = {};
 
 export default function App(props: Props) {
-  const [activeFileIndex, setActiveFileIndex] = useState(0);
+  const [activeFile, setActiveFile] = useState(files[0].file);
   const [mapData, setMapData] = useState<JSON | undefined>(undefined);
   const [displayMapSelector, setDisplayMapSelector] = useState(false);
+  const [displayMapMenu, setDisplayMapMenu] = useState(false);
   const [mapVisits, setMapVisits] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -45,8 +53,15 @@ export default function App(props: Props) {
     setMapData(data);
   };
 
+  const handleMapMenuClick = useCallback(file => {
+    setActiveFile(file);
+    setDisplayMapMenu(false);
+    setDisplayMapSelector(false);
+  }, []);
+
   useEffect(() => {
     setDisplayMapSelector(false);
+    setDisplayMapMenu(false);
     setIsLoading(false);
   }, [mapData]);
 
@@ -64,37 +79,49 @@ export default function App(props: Props) {
       {displayMapSelector && mapVisits === 0 && (
         <CustomDialog content="Drag around the map to select the area to render. Click once to start drawing the area. Click once more to end drawing."></CustomDialog>
       )}
-      {displayMapSelector && (
-        <>
-          <ButtonToIsometric
-            className="switch-view"
-            onClick={() => {
-              setDisplayMapSelector(false);
-            }}
-          >
-            Isometric 3d View &gt;
-          </ButtonToIsometric>
-          <MapSelector
-            onFetchDataInit={() => {
-              setIsLoading(true);
-            }}
-            onFetchData={onMapData}
-          />
-        </>
+      {(displayMapSelector || displayMapMenu) && (
+        <ButtonToIsometric
+          className="switch-view"
+          onClick={() => {
+            setDisplayMapSelector(false);
+            setDisplayMapMenu(false);
+          }}
+        >
+          Isometric 3d View &gt;
+        </ButtonToIsometric>
       )}
-      {!displayMapSelector && (
+      {displayMapMenu && (
+        <MapMenu areas={config} onClickCallback={handleMapMenuClick}></MapMenu>
+      )}
+      {displayMapSelector && (
+        <MapSelector
+          onFetchDataInit={() => {
+            setIsLoading(true);
+          }}
+          onFetchData={onMapData}
+        />
+      )}
+      {!displayMapSelector && !displayMapMenu && (
         <>
           <ButtonToMapSelector
             className="switch-view inverted"
-            onClick={() => {
+            /*onClick={() => {
               setDisplayMapSelector(true);
+            }}*/
+          >
+            &lt; Map selector
+          </ButtonToMapSelector>
+          <ButtonToMapMenu
+            className="switch-view inverted"
+            onClick={() => {
+              setDisplayMapMenu(true);
             }}
           >
             &lt; Map
-          </ButtonToMapSelector>
+          </ButtonToMapMenu>
           <CanvasContainer
-            key={`mapData${activeFileIndex}`}
-            file={files[activeFileIndex].file}
+            key={`mapData${activeFile}`}
+            file={activeFile}
             data={mapData}
           />
         </>
